107c4537e32efe489473f013f253648e86ca9f63
Jewel-190237 <1902378@ku.ac.bd>
2025-05-18 22:35:38 +0600

    code updated

---
+++ src/app/doctor-data/page.js
@@ -0,0 +1,10 @@
+import DoctorData from "../../components/DoctorData"
+const DoctorAndDiseasesData = () => {
+  return (
+    <div>
+      <DoctorData />
+    </div>
+  )
+}
+
+export default DoctorAndDiseasesData
