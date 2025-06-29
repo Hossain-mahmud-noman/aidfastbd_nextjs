79a97137ddd0abce56994ac7f5fcdf34c641cbe0
Jewel-190237 <1902378@ku.ac.bd>
2025-06-29 21:44:28 +0600

    code updated

--- src/components/information/information.js
+++ src/components/information/information.js
@@ -1,9 +1,6 @@
 'use client'
 
 import Image from "next/image"
-import Link from "next/link"
-import { FaArrowRightLong } from "react-icons/fa6"
-
 
 const Information = () => {
    const data = [
--- src/components/service/service.js
+++ src/components/service/service.js
@@ -4,68 +4,79 @@ import Image from "next/image"
 import Link from "next/link"
 import { FaArrowRightLong } from "react-icons/fa6"
 
-
 const Service = () => {
    const data = [
       {
-         "image": "/services/doctor.png",
-         "heading": "General Physicians",
-         "description": "Consult experienced doctors for primary health concerns."
+         "image": "/home/service/s1.png",
+         "heading": "Appoinment of Doctor",
+         "description": "Consult experienced doctors for primary health concerns.",
+         "link": "/doctor"
       },
       {
-         "image": "/services/ambulance.png",
-         "heading": "Ambulance Services",
-         "description": "Get emergency ambulance support anywhere in the country."
+         "image": "/home/service/s2.png",
+         "heading": "Diagonistick Center",
+         "description": "Get emergency ambulance support anywhere in the country.",
+         "link": "/diagonistick"
       },
       {
-         "image": "/services/medicine.png",
-         "heading": "Pharmacy Delivery",
-         "description": "Order medicines online and receive them at your doorstep."
+         "image": "/home/service/s3.png",
+         "heading": "Dental Clinik",
+         "description": "Order medicines online and receive them at your doorstep.",
+         "link": "/doctor"
       },
       {
-         "image": "/services/lab-test.png",
-         "heading": "Lab Tests",
-         "description": "Book lab tests and get reports from trusted labs."
-      },
-      {
-         "image": "/services/telemedicine.png",
-         "heading": "Telemedicine",
-         "description": "Consult doctors via video call from the comfort of your home."
-      },
-      {
-         "image": "/services/diagnostics.png",
-         "heading": "Diagnostic Centers",
-         "description": "Access top diagnostic centers for accurate health checkups."
-      },
-      {
-         "image": "/services/nursing.png",
-         "heading": "Home Nursing",
-         "description": "Hire professional nursing care at home for patients."
-      },
-      {
-         "image": "/services/counseling.png",
-         "heading": "Mental Health Support",
-         "description": "Get counseling and support for mental wellness."
-      },
-      {
-         "image": "/services/physio.png",
-         "heading": "Physiotherapy",
-         "description": "Book physical therapy sessions at clinics or home."
-      },
-      {
-         "image": "/services/health-insurance.png",
-         "heading": "Health Insurance",
-         "description": "Explore affordable health insurance plans."
-      },
-      {
-         "image": "/services/blood-bank.png",
+         "image": "/home/service/s4.png",
          "heading": "Blood Bank",
-         "description": "Find and request blood donors quickly and reliably."
+         "description": "Book lab tests and get reports from trusted labs.",
+         "link": "/doctor"
       },
       {
-         "image": "/services/medical-equip.png",
-         "heading": "Medical Equipment",
-         "description": "Rent or purchase essential medical equipment."
+         "image": "/home/service/s5.png",
+         "heading": "Ambulence Service",
+         "description": "Consult doctors via video call from the comfort of your home.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s6.png",
+         "heading": "Pharmacy Service",
+         "description": "Access top diagnostic centers for accurate health checkups.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s7.png",
+         "heading": "Physiotherapy Center",
+         "description": "Hire professional nursing care at home for patients.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s8.png",
+         "heading": "Eye care Center",
+         "description": "Get counseling and support for mental wellness.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s9.png",
+         "heading": "Drug Rehabilitation Center",
+         "description": "Book physical therapy sessions at clinics or home.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s10.png",
+         "heading": "Nursing Care home",
+         "description": "Explore affordable health insurance plans.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s11.png",
+         "heading": "Haring Care Center",
+         "description": "Find and request blood donors quickly and reliably.",
+         "link": "/doctor"
+      },
+      {
+         "image": "/home/service/s12.png",
+         "heading": "Emmergency Service",
+         "description": "Rent or purchase essential medical equipment.",
+         "link": "/doctor"
       }
    ]
 
@@ -82,23 +93,26 @@ const Service = () => {
             </Link>
          </div>
          <div className="mt-8 md:mt-12 lg:mt-14 xl:mt-[70px]">
-            <div className="grid lg:grid-cols-3 grid-cols-2 xl:gap-6 lg:gap-5 md:gap-4 gap-2">
-               {
-                  data.map((Item, index) => (
-                     <div key={index} className="border border-[#3056D321] bg-[#FFFFFF] xl:rounded-2xl lg:rounded-xl rounded-lg xl:py-8 lg:py-7 py-5 lg:px-5 px-4 mx-auto">
-                        <Image
-                           src={Item.image}
-                           width={100}
-                           height={100}
-                           className="w-12 h-12 lg:w-14 lg:h-14 xl:w-[76px] xl:h-[76px] object-fill mx-auto"
-                           alt="Service image"
-                        />
-                        <h4 className="heading3 mt-4 md:mt-5 xl:mt-6 text-[#061C3D] text-center">{Item.heading}</h4>
-                        <h4 className="description1 mt-2 md:mt-3 text-[#42526B] text-center">{Item.description}</h4>
-                     </div>
-                  ))
-               }
+            <div className="grid lg:grid-cols-3 grid-cols-2 xl:gap-6 lg:gap-5 md:gap-4 gap-4">
+               {data.map((item, index) => (
+                  <Link
+                     href={item.link}
+                     key={index}
+                     className="flex flex-col justify-start items-center border border-[#3056D321] bg-white xl:rounded-2xl lg:rounded-xl rounded-lg p-6 mx-auto w-full text-center"
+                  >
+                     <Image
+                        src={item.image}
+                        width={76}
+                        height={76}
+                        className="object-contain"
+                        alt="Service image"
+                     />
+                     <h4 className="heading3 mt-6 text-[#061C3D]">{item.heading}</h4>
+                     <p className="description1 mt-3 text-[#42526B]">{item.description}</p>
+                  </Link>
+               ))}
             </div>
+
          </div>
       </section>
    )
