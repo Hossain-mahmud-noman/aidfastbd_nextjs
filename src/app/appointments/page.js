41aef59830ef6b75fa82c59c7bd88e6d5e1d68f9
Jewel-190237 <1902378@ku.ac.bd>
2025-08-06 16:24:43 +0600

    appoinment updatd

--- src/app/appointments/page.js
+++ src/app/appointments/page.js
@@ -3,22 +3,22 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { appname } from "../../utils/constants";
 import AppointmentClient from './AppointmentClient'
+// import { useAuth } from "/context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 export const metadata = {
   title: "Appointments | " + appname,
 };
 
 async function page() {
   const tokenCookie = cookies().get("token")?.value ?? "";
-  const userCookie = cookies().get("user")?.value;
-  const user = userCookie ? JSON.parse(userCookie) : null;
-
-  if (!tokenCookie || !user) {
+  const { user } = useAuth;
+  if (!user) {
     redirect("/login");
   }
 
   return (
     <div className="pt-16 pl-2 pr-2 mt-3">
-      <AppointmentClient token={tokenCookie}  />
+      <AppointmentClient token={tokenCookie} />
     </div>
   );
 }
