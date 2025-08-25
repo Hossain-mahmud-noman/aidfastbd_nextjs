fe309b911327f43a26e5a0b63c1365b039cbe41c
Jewel-190237 <1902378@ku.ac.bd>
2025-08-25 22:45:05 +0600

    consultation updated

--- src/components/landing/consultation/consultation.js
+++ src/components/landing/consultation/consultation.js
@@ -64,14 +64,14 @@ const Consultation = () => {
 
    return (
       <section className="mt-10 md:mt-14 lg:mt-20 xl:mt-20  rouonded-[12px] relative max-w-[1320px] mx-auto hero">
-         <div className="aid-container py-6 md:py-7 lg:py-8 xl:py-10 w-full">
+         <div className="aid-container py-3 md:py-5 lg:py-8 xl:py-10 w-full">
 
             {/* Prev Button */}
             <button
                onClick={() => swiperInstance?.slidePrev()}
                className="swipper-button-left z-50 absolute left-3 top-1/2 -translate-y-1/2"
             >
-               <GoArrowLeft size={24} />
+               <GoArrowLeft className="text-sm md:text-base lg:text-lg xl:text-xl" />
             </button>
 
             <div className="w-full bg-[#E6F4FF] rounded-xl lg:rounded-[20px] relative">
@@ -109,24 +109,24 @@ const Consultation = () => {
                                  width={500}
                                  height={500}
                                  alt="consultation"
-                                 className="w-full md:w-[277px] h-[184px] md:h-[234px] object-fill md:pl-4 lg:pl-10 xl:pl-12 pt-3 lg:pt-4"
+                                 className="w-[108px] md:w-[200px] lg:w-[250px] xl:w-[290px] h-[91px] md:h-[134px] lg:h-[230px] xl:h-[250px] object-fill md:pl-4 lg:pl-10 xl:pl-12 pt-3 lg:pt-4"
                               />
                            </div>
-                           <div className="py-5 md:py-0">
-                              <p className="description1 text-[#474747] ">{i18n.t(item.heading)}</p>
-                              <h3 className="mt-3 heading4 text-[#000000] ">
+                           <div className="py-3 md:py-0">
+                              <p className="service-button text-[#474747] ">{i18n.t(item.heading)}</p>
+                              <h3 className="md:mt-2 lg:mt-3 cons-heading text-[#000000] ">
                                  {i18n.t(item.description.before)}
                                  <del className="text-red-500 ml-1">{i18n.t(item.description.oldPrice)}</del>,
                                  <span className="text-green-600 font-semibold">{i18n.t(item.description.newPrice)}</span>
                               </h3>
 
-                              <div className="mt-5 md:mt-6 lg:mt-7 xl:mt-[30px]">
+                              <div className="mt-2 md:mt-4 lg:mt-7 xl:mt-[30px]">
                                  <button
                                     type="button"
-                                    className="bg-[#1087EF] px-4 py-2 md:px-6 md:py-3 lg:px-8 lg:py-4  rounded-full description1 hover:bg-blue-700 transition-all duration-300 flex items-center gap-2"
+                                    className="bg-[#1087EF] px-2.5 py-1 md:px-6 md:py-3 lg:px-8 lg:py-4 rounded-full description1 hover:bg-blue-700 transition-all duration-300 flex items-center gap-2"
                                  >
-                                    <p className="description2 text-white">{i18n.t("Begin Live Consultation")}</p>
-                                    <FaArrowRightLong className="text-white description2" />
+                                    <p className="service-button text-white">{i18n.t("Begin Live Consultation")}</p>
+                                    <FaArrowRightLong className="text-white service-button" />
                                  </button>
                               </div>
                            </div>
@@ -141,7 +141,7 @@ const Consultation = () => {
                onClick={() => swiperInstance?.slideNext()}
                className="swipper-button-right z-50 absolute right-3 top-1/2 -translate-y-1/2"
             >
-               <GoArrowRight size={24} />
+               <GoArrowRight className="text-sm md:text-base lg:text-lg xl:text-xl" />
             </button>
 
             <div ref={paginationRef} className="custom-pagination py-4 flex justify-center"></div>
