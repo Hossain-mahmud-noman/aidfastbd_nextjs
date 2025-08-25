633f134a143991edd28c741538a7a41831c40862
Jewel-190237 <1902378@ku.ac.bd>
2025-08-25 09:31:47 +0600

    hero updated

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -73,35 +73,34 @@ const Hero = () => {
             >
                {data.map((item, index) => (
                   <SwiperSlide key={index} >
-                     <div className="py-8 md:py-10 lg:py-[55px] aid-container mx-auto flex flex-col md:flex-row items justify-between gap-8 md:gap-2 lg:gap-4 xl:gap-10 items-center overflow-hidden">
-                        <div className='xl:pl-10 lg:pl-8 pl-5'>
-                           <span className=" description2 text-[#000000] font-semibold bg-[#B1CFEA2E] px-3 py-1 rounded-md inline-block mb-4">
+                     <div className="border py-4 md:py-10 lg:py-[55px] aid-container mx-auto flex items justify-between gap-3 md:gap-2 lg:gap-4 xl:gap-10 items-center overflow-hidden">
+                        <div className='border'>
+                           <span className="hero-description text-[#000000] font-semibold bg-[#B1CFEA2E] px-2 py-1 md:px-4 md:py-2.4 rounded-md inline-block">
                               {i18n.t(item.heading)}
                            </span>
 
-                           <h1 className="heading2 text-[#212121] mt-5 lg:mt-6 capitalize">
+                           <h1 className="hero-heading text-[#212121] mt-3 md:mt-5 lg:mt-6 capitalize">
                               {i18n.t(item.title)}
                            </h1>
-                           <div className="mt-7 md:mt-8 lg:mt-10 xl:mt-12 flex items-center flex-col md:flex-row gap-3 md:gap-4 xl:gap-5">
-
+                           <div className="mt-3 md:mt-5 lg:mt-10 xl:mt-12 flex items-center gap-2 md:gap-3 lg:gap-4 xl:gap-5">
                               <Link
                                  href="https://play.google.com/store/apps/details?id=com.aidfastbd.app"
-                                 className="relative w-[150px] h-[50px]"
                               >
                                  <Image
                                     src="/home/service/google.png"
-                                    fill
+                                    width={500}
+                                    height={500}
                                     alt="Google Play"
-                                    className="object-contain"
+                                    className="w-[76px] h-[30px] lg:w-[150px] xl:w-[160px] md:h-[56px] md:w-[140px] object-contain"
                                  />
                               </Link>
 
                               <Link
                                  href="https://play.google.com/store/apps/details?id=com.aidfastbd.app"
-                                 className="hover:scale-105 bg-[#1087EF] px-6 py-3 rounded-[12px] description2 hover:bg-blue-700 transition-all duration-300 flex items-center justify-center gap-2"
+                                 className="hover:scale-105 bg-[#1087EF] px-3 md:px-6 py-1 md:py-3 rounded-[12px] description2 hover:bg-blue-700 transition-all duration-300 flex items-center justify-center gap-2"
                               >
-                                 <FiDownload className="text-white description1" />
-                                 <p className="description2 text-white">{i18n.t("Download Now")}</p>
+                                 <FiDownload className="text-white hero-description" />
+                                 <p className="hero-description text-white">{i18n.t("Download Now")}</p>
                               </Link>
                            </div>
                         </div>
@@ -112,7 +111,7 @@ const Hero = () => {
                               width={1000}
                               height={1000}
                               alt="mobile"
-                              className="w-[220px] md:w-[233px] xl:h-[488px] lg:h-[380px] h-[400px] object-fill"
+                              className="w-[64px] h-[137px] md:w-[143px] md:h-[300px] lg:w-[172px] lg:h-[361px] object-fill"
                            />
                         </div>
                      </div>
