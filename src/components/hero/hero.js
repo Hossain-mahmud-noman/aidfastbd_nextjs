ca67a6ba4909c7f67dee94d6116da02a0fe1ec60
Jewel-190237 <190237@ku.ac.bd>
2025-09-11 01:14:41 +0600

    code updated

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -69,54 +69,11 @@ const Hero = () => {
         >
           {data.map((item, index) => (
             <SwiperSlide key={index}>
-              {/* <div className="py-4 md:py-10 lg:py-[55px] aid-container mx-auto flex items justify-between gap-3 md:gap-2 lg:gap-4 xl:gap-10 items-center overflow-hidden">
-                        <div className=''>
-                           <span className="hero-description text-[#000000] font-semibold bg-[#B1CFEA2E] px-2 py-1 md:px-4 md:py-2.4 rounded-md inline-block">
-                              {i18n.t(item.heading)}
-                           </span>
-
-                           <h1 className="hero-heading text-[#212121] mt-3 md:mt-5 lg:mt-6 capitalize">
-                              {i18n.t(item.title)}
-                           </h1>
-                           <div className="mt-3 md:mt-5 lg:mt-10 xl:mt-12 flex items-center gap-2 md:gap-3 lg:gap-4 xl:gap-5">
-                              <Link
-                                 href="https://play.google.com/store/apps/details?id=com.aidfastbd.app"
-                              >
-                                 <Image
-                                    src="/home/service/google.png"
-                                    width={500}
-                                    height={500}
-                                    alt="Google Play"
-                                    className="w-[76px] h-[30px] lg:w-[150px] xl:w-[160px] md:h-[56px] md:w-[140px] object-contain"
-                                 />
-                              </Link>
-
-                              <Link
-                                 href="https://play.google.com/store/apps/details?id=com.aidfastbd.app"
-                                 className="hover:scale-105 bg-[#1087EF] px-3 md:px-6 py-1 md:py-3 rounded-[12px] description2 hover:bg-blue-700 transition-all duration-300 flex items-center justify-center gap-2"
-                              >
-                                 <FiDownload className="text-white hero-description" />
-                                 <p className="hero-description text-white">{i18n.t("Download Now")}</p>
-                              </Link>
-                           </div>
-                        </div>
-
-                        <div className="relative flex justify-end items-center">
-                           <Image
-                              src={item.image}
-                              width={1000}
-                              height={1000}
-                              alt="mobile"
-                              className="w-[64px] h-[137px] md:w-[143px] md:h-[300px] lg:w-[172px] lg:h-[361px] object-fill"
-                           />
-                        </div>
-                     </div> */}
               <div
                 className="w-full"
                 onClick={() =>
-                  router.push(
-                    "https://play.google.com/store/apps/details?id=com.aidfastbd.app"
-                  )
+                  (window.location.href =
+                    "https://play.google.com/store/apps/details?id=com.aidfastbd.app")
                 }
               >
                 <Image
